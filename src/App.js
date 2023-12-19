d1a7cadceba5535991614db778e2315c563753d1
PcMain <hital12.ci@gmail.com>
2023-12-18 21:59:10 -0300

    Poniendolo todo junto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
-import Card from "./componentes/Component.js"
+import Card  from "./componentes/Component.js"
 
 function App() {
   return (
     <div>
-      <Card firstName="John" lastName="Doe" age={45} hairColor="Black" />
-      <Card firstName="John" lastName="Smith" age={88} hairColor="Brown" />
-      <Card firstName="Fillmore" lastName="Millard" age={50} hairColor="Brown" />
-      <Card firstName="Smith" lastName="Maria" age={62} hairColor="Brown" />
+      <Card  firstName="John" lastName="Doe" age={45} hairColor="Black" />
+      <Card  firstName="John" lastName="Smith" age={88} hairColor="Brown" />
+      <Card  firstName="Fillmore" lastName="Millard" age={50} hairColor="Brown" />
+      <Card  firstName="Smith" lastName="Maria" age={62} hairColor="Brown" />
     </div>
   );
 }
--- src/componentes/Component.js
+++ src/componentes/Component.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: props.age,
+    };
+  }
 
-function Card(props) {
-  return (
-    <div>
-      <h3>{props.lastName}, {props.firstName}</h3>
-      <p>Age: {props.age}</p>
-      <p>Hair Color: {props.hairColor}</p>
-    </div>
-  );
+  cumple = () => {
+    this.setState((prevState) => ({ age: prevState.age + 1 }));
+  };
+
+  render() {
+    return (
+      <div className="card">
+        <h3>
+          {this.props.lastName}, {this.props.firstName}
+        </h3>
+        <p>Age: {this.state.age}</p>
+        <p>Hair Color: {this.props.hairColor}</p>
+        <button onClick={this.cumple}>
+          Birthday Button for {this.props.firstName} {this.props.lastName}
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Card
+export default Card;
